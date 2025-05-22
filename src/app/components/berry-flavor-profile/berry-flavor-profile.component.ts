513adeade4c5014940753c658eabc60c5dc3bba5
DanielSerio <dserio.d@gmail.com>
2025-05-22 12:12:30 -0400

    flavor profile base styles

--- src/app/components/berry-flavor-profile/berry-flavor-profile.component.ts
+++ src/app/components/berry-flavor-profile/berry-flavor-profile.component.ts
@@ -1,17 +1,26 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
+import { IonicModule } from "@ionic/angular";
 
 @Component({
   selector: 'app-berry-flavor-profile',
   templateUrl: './berry-flavor-profile.component.html',
   styleUrls: ['./berry-flavor-profile.component.scss'],
   standalone: true,
-  imports: [CommonModule]
+  imports: [IonicModule, CommonModule]
 })
 export class BerryFlavorProfileComponent implements OnInit {
   @Input({ required: true }) profile!: Record<string, number>;
+
+  entries!: [string, number][];
+
   constructor() { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.entries = Object.entries(this.profile);
+  }
 
+  getWidthStyle(n: number) {
+    return `width: ${n * 100}%`;
+  }
 }
